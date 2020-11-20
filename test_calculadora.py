0ae78cf2f2afe0aaab3188dfd37e7821427d63a4
David Vargas <davidvgtta@gmail.com>
2020-11-19 22:18:47 -0400

    commit 5: AttributeError: 'Calculator' object has no attribute 'add'

--- test_calculadora.py
+++ test_calculadora.py
@@ -9,3 +9,10 @@ class TestMyCalculator(unittest.TestCase):
 
     def test_initial_value(self):
         self.assertEqual(0, self.calc.value)
+
+    # Creamos un nuevo test para comprobar una suma
+    def test_add_method(self):
+        # Ejecutamos el método
+        self.calc.add(1, 3)
+        # Comprobamos si el valor es el que esperamos
+        self.assertEqual(4, self.calc.value)
