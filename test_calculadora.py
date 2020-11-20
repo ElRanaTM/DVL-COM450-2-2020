4fa3b31af395ae6763e61c6d8c129a86c2b70ecc
David Vargas <davidvgtta@gmail.com>
2020-11-19 22:16:39 -0400

    commit 4: Ran 1 test in 0.001s

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,10 +1,11 @@
 import unittest
-
-# Importamos la clase calculadora
 from calculadora import Calculadora
+
 
 class TestMyCalculator(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculadora()
+
     def test_initial_value(self):
-        calc = Calculadora()
-        self.assertEqual(0, calc.value)
+        self.assertEqual(0, self.calc.value)
